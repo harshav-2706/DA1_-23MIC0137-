61b2509571350c6c7e86d074018807b1e8a54012
harsha <harsha@localhost>
2026-01-11 19:33:04 +0530

    first commit

---
+++ src/ci/StudentInfo.java
@@ -0,0 +1,10 @@
+package ci;
+
+public class StudentInfo {
+	public static void main(String[] args) {
+        System.out.println("Student Name: Harsha V");
+        System.out.println("Register Number: 23MIC0137");
+        System.out.println("Message: Jenkins–GitHub Integration Successful");
+    }
+
+}
