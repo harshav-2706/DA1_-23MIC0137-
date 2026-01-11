f7903539dccb13f43d643735f9d2b8a8e015c72c
harsha <harsha@localhost>
2026-01-11 19:43:47 +0530

    second commit

--- src/ci/StudentInfo.java
+++ src/ci/StudentInfo.java
@@ -5,6 +5,7 @@ public class StudentInfo {
         System.out.println("Student Name: Harsha V");
         System.out.println("Register Number: 23MIC0137");
         System.out.println("Message: Jenkins–GitHub Integration Successful");
+        System.out.println("Mtech integrated CSE");
     }
 
 }
